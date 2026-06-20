7b2919837bda9cd410f9b54c83422b3f0288fd79
Mario <mario@192.168.1.6>
2026-06-21 03:57:50 +0700

    add ci cd pipeline

---
+++ .dockerfile
@@ -0,0 +1,6 @@
+FROM node:18
+
+WORKDIR /app
+COPY . .
+
+CMD ["node", "app.js"]
---
+++ app.js
@@ -0,0 +1,6 @@
+// app.js
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.end("Hello DevOps World 🚀");
+}).listen(3000);
